f02e782e85f69b849fe0c739450dfbad6936799b
Kim Gonzales <kimgo589@gmail.com>
2017-12-10 02:58:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,18 +13,18 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(`${string}`.toLowerCase())
 }
-var uppercase ="HELLO!"
-var lowercase = 'hello!'
-var mixedCase = "Hi there!"
 
 function sayHiToGrandma(string) {
-  if ('${string}'.toLowerCase() === lowercase) {
-    return ("I can't hear you!")}
-
-  else if('${string}'.toUpperCase() === uppercase){
-    return ("YES INDEED!")}
-    
-  else (`${string}` === mixedCase){
-    return ("I love you, too")
+  if (string.toLowerCase() === lowercase) {
+    return "I can't hear you!"
   }
+
+  if (string.toUpperCase() === uppercase) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
 }
