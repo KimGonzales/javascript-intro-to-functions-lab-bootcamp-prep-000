1068f0604f4c5ad7648d8a48a542f45ac0842a7d
Kim Gonzales <kimgo589@gmail.com>
2017-12-10 02:25:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if ('${string}'.toLowerCase === lowercase)
+  if ('${string}'.toLowerCase() === lowercase){
+    return ("I can't hear you!")
+  }
+  else if (`${string}`.toUpperCase() === uppercase){
+    return ("YES INDEED!")
+    }
+  else (`${string}` === "I love you, Grandma.")
+    return("I love you, too")
 }
